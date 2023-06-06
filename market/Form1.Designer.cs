945e110973ca5b0e286abc61fdb3a24d602c16b4
davilson <daviventurini7@gmail.com>
2023-06-06 17:15:30 -0300

    foi a braba

--- market/Form1.Designer.cs
+++ market/Form1.Designer.cs
@@ -28,17 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
+            this.Estoque = new System.Windows.Forms.Button();
+            this.Vendas = new System.Windows.Forms.Button();
+            this.Produtos = new System.Windows.Forms.Button();
+            this.Cadastro = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -46,63 +50,45 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel1.Location = new System.Drawing.Point(-10, -37);
+            this.panel1.Controls.Add(this.Cadastro);
+            this.panel1.Controls.Add(this.Produtos);
+            this.panel1.Controls.Add(this.Vendas);
+            this.panel1.Controls.Add(this.Estoque);
+            this.panel1.Location = new System.Drawing.Point(286, -38);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(197, 495);
+            this.panel1.Size = new System.Drawing.Size(1391, 91);
             this.panel1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(194, 13);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 84);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 16);
+            this.label1.Size = new System.Drawing.Size(108, 25);
             this.label1.TabIndex = 1;
             this.label1.Text = "Dashboard";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // dateTimePicker1
             // 
             this.dateTimePicker1.Location = new System.Drawing.Point(309, 13);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(121, 22);
+            this.dateTimePicker1.Size = new System.Drawing.Size(264, 22);
             this.dateTimePicker1.TabIndex = 2;
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(457, 5);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(69, 32);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Ok";
-            this.button1.UseVisualStyleBackColor = false;
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(532, 5);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(69, 32);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Ok";
-            this.button2.UseVisualStyleBackColor = false;
             // 
             // chart1
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
-            this.chart1.Location = new System.Drawing.Point(225, 84);
+            chartArea6.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart1.Legends.Add(legend6);
+            this.chart1.Location = new System.Drawing.Point(286, 84);
             this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart1.Series.Add(series2);
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.chart1.Series.Add(series6);
             this.chart1.Size = new System.Drawing.Size(455, 168);
             this.chart1.TabIndex = 5;
             this.chart1.Text = "chart1";
@@ -111,9 +97,9 @@
             // 
             this.panel2.BackColor = System.Drawing.SystemColors.ControlLight;
             this.panel2.Controls.Add(this.label2);
-            this.panel2.Location = new System.Drawing.Point(225, 285);
+            this.panel2.Location = new System.Drawing.Point(300, 327);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 173);
+            this.panel2.Size = new System.Drawing.Size(200, 193);
             this.panel2.TabIndex = 6;
             // 
             // label2
@@ -125,20 +111,79 @@
             this.label2.TabIndex = 0;
             this.label2.Text = "Informações";
             // 
+            // Estoque
+            // 
+            this.Estoque.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Estoque.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Estoque.Location = new System.Drawing.Point(563, 46);
+            this.Estoque.Name = "Estoque";
+            this.Estoque.Size = new System.Drawing.Size(112, 35);
+            this.Estoque.TabIndex = 7;
+            this.Estoque.Text = "Estoque";
+            this.Estoque.UseVisualStyleBackColor = false;
+            // 
+            // Vendas
+            // 
+            this.Vendas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Vendas.Font = new System.Drawing.Font("Mongolian Baiti", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Vendas.Location = new System.Drawing.Point(445, 46);
+            this.Vendas.Name = "Vendas";
+            this.Vendas.Size = new System.Drawing.Size(112, 35);
+            this.Vendas.TabIndex = 8;
+            this.Vendas.Text = "Vendas";
+            this.Vendas.UseVisualStyleBackColor = false;
+            // 
+            // Produtos
+            // 
+            this.Produtos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Produtos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Produtos.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Produtos.Font = new System.Drawing.Font("Mongolian Baiti", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Produtos.Location = new System.Drawing.Point(327, 46);
+            this.Produtos.Name = "Produtos";
+            this.Produtos.Size = new System.Drawing.Size(112, 35);
+            this.Produtos.TabIndex = 9;
+            this.Produtos.Text = "Produtos";
+            this.Produtos.UseVisualStyleBackColor = false;
+            // 
+            // Cadastro
+            // 
+            this.Cadastro.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.Cadastro.Font = new System.Drawing.Font("Mongolian Baiti", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cadastro.Location = new System.Drawing.Point(753, 46);
+            this.Cadastro.Name = "Cadastro";
+            this.Cadastro.Size = new System.Drawing.Size(112, 35);
+            this.Cadastro.TabIndex = 10;
+            this.Cadastro.Text = "Cadastro\r\n";
+            this.Cadastro.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Sylfaen", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(216, 36);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "DeEDU Market.\r\n";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(1339, 725);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.chart1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
@@ -152,11 +197,14 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Estoque;
+        private System.Windows.Forms.Button Produtos;
+        private System.Windows.Forms.Button Vendas;
+        private System.Windows.Forms.Button Cadastro;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- market/Form1.cs
+++ market/Form1.cs
@@ -7,14 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using market;
 namespace market
+
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Width = 100;
+            Height = 100;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
