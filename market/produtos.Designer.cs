d51265079e40a7243c93abc3cb4456cdb27f40f1
davilson <daviventurini7@gmail.com>
2023-06-14 16:33:28 -0300

    conclusão da página de cadastros de produtos. altava só refinar o ambiente

--- market/produtos.Designer.cs
+++ market/produtos.Designer.cs
@@ -33,18 +33,21 @@
             this.txtNome = new System.Windows.Forms.TextBox();
             this.txtPreco = new System.Windows.Forms.TextBox();
             this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(32, 30);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(855, 522);
+            this.dataGridView1.Size = new System.Drawing.Size(457, 522);
             this.dataGridView1.TabIndex = 2;
             // 
             // btnCadastrar
@@ -80,10 +83,24 @@
             this.txtQuantidade.TabIndex = 7;
             this.txtQuantidade.Text = "Quantidade";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft New Tai Lue", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(571, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 32);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Cadastro de Produtos";
+            // 
             // Produtos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtQuantidade);
             this.Controls.Add(this.txtPreco);
             this.Controls.Add(this.txtNome);
@@ -103,5 +120,6 @@
         private System.Windows.Forms.TextBox txtNome;
         private System.Windows.Forms.TextBox txtPreco;
         private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.Label label1;
     }
 }
