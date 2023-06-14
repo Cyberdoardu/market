be75b853619ae56d30347b7f04d9f472e0d3de06
BuildTools <unconfigured@null.spigotmc.org>
2023-06-14 18:31:14 -0300

    Polimorfismo Cliente e funcionário

--- market/Cadastro.Designer.cs
+++ market/Cadastro.Designer.cs
@@ -29,24 +29,141 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.dtDataNascimento = new System.Windows.Forms.DateTimePicker();
+            this.rdbCliente = new System.Windows.Forms.RadioButton();
+            this.rdbFuncionario = new System.Windows.Forms.RadioButton();
+            this.dgvFuncionarios = new System.Windows.Forms.DataGridView();
+            this.dgvClientes = new System.Windows.Forms.DataGridView();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnDescartar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFuncionarios)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(81, 56);
+            this.label1.Location = new System.Drawing.Point(26, 19);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(62, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Cadastro";
             // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(56, 588);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(228, 22);
+            this.txtNome.TabIndex = 1;
+            // 
+            // dtDataNascimento
+            // 
+            this.dtDataNascimento.Location = new System.Drawing.Point(321, 588);
+            this.dtDataNascimento.Name = "dtDataNascimento";
+            this.dtDataNascimento.Size = new System.Drawing.Size(200, 22);
+            this.dtDataNascimento.TabIndex = 0;
+            // 
+            // rdbCliente
+            // 
+            this.rdbCliente.AutoSize = true;
+            this.rdbCliente.Location = new System.Drawing.Point(555, 588);
+            this.rdbCliente.Name = "rdbCliente";
+            this.rdbCliente.Size = new System.Drawing.Size(69, 20);
+            this.rdbCliente.TabIndex = 2;
+            this.rdbCliente.TabStop = true;
+            this.rdbCliente.Text = "Cliente";
+            this.rdbCliente.UseVisualStyleBackColor = true;
+            // 
+            // rdbFuncionario
+            // 
+            this.rdbFuncionario.AutoSize = true;
+            this.rdbFuncionario.Location = new System.Drawing.Point(555, 614);
+            this.rdbFuncionario.Name = "rdbFuncionario";
+            this.rdbFuncionario.Size = new System.Drawing.Size(104, 20);
+            this.rdbFuncionario.TabIndex = 3;
+            this.rdbFuncionario.TabStop = true;
+            this.rdbFuncionario.Text = "Colaborador";
+            this.rdbFuncionario.UseVisualStyleBackColor = true;
+            // 
+            // dgvFuncionarios
+            // 
+            this.dgvFuncionarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFuncionarios.Location = new System.Drawing.Point(29, 328);
+            this.dgvFuncionarios.Name = "dgvFuncionarios";
+            this.dgvFuncionarios.RowHeadersWidth = 51;
+            this.dgvFuncionarios.RowTemplate.Height = 24;
+            this.dgvFuncionarios.Size = new System.Drawing.Size(866, 218);
+            this.dgvFuncionarios.TabIndex = 4;
+            // 
+            // dgvClientes
+            // 
+            this.dgvClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClientes.Location = new System.Drawing.Point(29, 75);
+            this.dgvClientes.Name = "dgvClientes";
+            this.dgvClientes.RowHeadersWidth = 51;
+            this.dgvClientes.RowTemplate.Height = 24;
+            this.dgvClientes.Size = new System.Drawing.Size(866, 218);
+            this.dgvClientes.TabIndex = 5;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(682, 590);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(213, 44);
+            this.btnCadastrar.TabIndex = 6;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(56, 652);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(111, 53);
+            this.btnSalvar.TabIndex = 7;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            // 
+            // btnDescartar
+            // 
+            this.btnDescartar.Location = new System.Drawing.Point(191, 652);
+            this.btnDescartar.Name = "btnDescartar";
+            this.btnDescartar.Size = new System.Drawing.Size(201, 53);
+            this.btnDescartar.TabIndex = 8;
+            this.btnDescartar.Text = "Descartar alterações";
+            this.btnDescartar.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(736, 667);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            // 
             // Cadastro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnDescartar);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.dgvClientes);
+            this.Controls.Add(this.dgvFuncionarios);
+            this.Controls.Add(this.rdbFuncionario);
+            this.Controls.Add(this.rdbCliente);
+            this.Controls.Add(this.dtDataNascimento);
+            this.Controls.Add(this.txtNome);
             this.Controls.Add(this.label1);
             this.Name = "Cadastro";
             this.Size = new System.Drawing.Size(916, 727);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFuncionarios)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -55,5 +172,15 @@
         #endregion
 
         public System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.DateTimePicker dtDataNascimento;
+        private System.Windows.Forms.RadioButton rdbCliente;
+        private System.Windows.Forms.RadioButton rdbFuncionario;
+        private System.Windows.Forms.DataGridView dgvFuncionarios;
+        private System.Windows.Forms.DataGridView dgvClientes;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnDescartar;
+        private System.Windows.Forms.Button button1;
     }
 }
--- market/Cadastro.cs
+++ market/Cadastro.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +15,283 @@ namespace market
 {
     public partial class Cadastro : UserControl
     {
+        private static Semaphore semaforo = new Semaphore(1, 1);
+
+        private DataTable dtClientes;
+        private DataTable dtFuncionarios;
+
         public Cadastro()
         {
             InitializeComponent();
+
+            dtClientes = new DataTable();
+            dtFuncionarios = new DataTable();
+
+            CarregarDados();
+        }
+
+        private void CarregarDados()
+        {
+            dtClientes.Clear();
+            dtFuncionarios.Clear();
+
+            Thread t1 = new Thread(() => CarregarPlanilha("Clientes", dtClientes));
+            Thread t2 = new Thread(() => CarregarPlanilha("Funcionarios", dtFuncionarios));
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+            dgvClientes.DataSource = dtClientes;
+            dgvFuncionarios.DataSource = dtFuncionarios;
+        }
+
+        private void CarregarPlanilha(string nomePlanilha, DataTable dt)
+        {
+            semaforo.WaitOne();
+
+            string pastaAppData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string pastaMarket = Path.Combine(pastaAppData, "market");
+            string filePath = Path.Combine(pastaMarket, "dados.xlsx");
+
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    throw new Exception("O arquivo dados.xlsx não foi encontrado.");
+                }
+
+                ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+
+                using (var package = new ExcelPackage(new FileInfo(filePath)))
+                {
+                    ExcelWorksheet planilha = package.Workbook.Worksheets[nomePlanilha];
+
+                    dt.Clear();
+                    dt.Columns.Clear();
+
+                    // Preencher o DataTable
+                    for (int i = planilha.Dimension.Start.Row; i <= planilha.Dimension.End.Row; i++)
+                    {
+                        DataRow row = dt.NewRow();
+                        for (int j = planilha.Dimension.Start.Column; j <= planilha.Dimension.End.Column; j++)
+                        {
+                            var cell = planilha.Cells[i, j];
+
+                            if (cell.Value != null)
+                            {
+                                if (i == planilha.Dimension.Start.Row)
+                                {
+                                    dt.Columns.Add(cell.Value.ToString());
+                                }
+                                else if (j <= dt.Columns.Count) // Verificando se a coluna existe no DataRow
+                                {
+                                    row[j - 1] = cell.Value;
+                                }
+                            }
+
+                        }
+
+                        if (i != planilha.Dimension.Start.Row)
+                        {
+                            dt.Rows.Add(row);
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                semaforo.Release();
+            }
+        }
+
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            Thread t1 = new Thread(() => SalvarPlanilha("Clientes", dtClientes));
+            Thread t2 = new Thread(() => SalvarPlanilha("Funcionarios", dtFuncionarios));
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+            MessageBox.Show("Dados salvos com sucesso!");
+        }
+
+        private void SalvarPlanilha(string nomePlanilha, DataTable dt)
+        {
+            semaforo.WaitOne();
+
+            string pastaAppData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string pastaMarket = Path.Combine(pastaAppData, "market");
+            string filePath = Path.Combine(pastaMarket, "dados.xlsx");
+
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    throw new Exception("O arquivo dados.xlsx não foi encontrado.");
+                }
+
+                ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+
+                using (var package = new ExcelPackage(new FileInfo(filePath)))
+                {
+                    ExcelWorksheet planilha = package.Workbook.Worksheets[nomePlanilha];
+
+                    // Limpar a planilha
+                    planilha.Cells.Clear();
+
+                    // Preencher a planilha com os dados do DataTable
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        for (int j = 0; j < dt.Columns.Count; j++)
+                        {
+                            if (i == 0)
+                            {
+                                planilha.Cells[i + 1, j + 1].Value = dt.Columns[j].ColumnName;
+                            }
+                            planilha.Cells[i + 2, j + 1].Value = dt.Rows[i][j];
+                        }
+                    }
+
+                    package.Save();
+                }
+            }
+            finally
+            {
+                semaforo.Release();
+            }
+        }
+
+        private void btnDescartar_Click(object sender, EventArgs e)
+        {
+            CarregarDados();
+        }
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            string nome = txtNome.Text;
+            DateTime dataNascimento = dtDataNascimento.Value;
+
+            Pessoa pessoa = null;
+            if (rdbCliente.Checked)
+            {
+                pessoa = new Cliente(nome, dataNascimento, 0, this);
+            }
+            else if (rdbFuncionario.Checked)
+            {
+                pessoa = new Funcionario(nome, dataNascimento, 0, "ativo", this);
+            }
+
+            if (pessoa == null)
+            {
+                throw new Exception("Nenhuma opção de cadastro selecionada");
+            }
+
+            Thread t = new Thread(pessoa.Cadastrar);
+            t.Start();
+        }
+
+
+
+        public abstract class Pessoa
+        {
+            public string Nome { get; set; }
+            public DateTime DataNascimento { get; set; }
+            protected Cadastro Cadastro { get; set; }
+
+            protected Pessoa(string nome, DateTime dataNascimento, Cadastro cadastro)
+            {
+                Nome = nome;
+                DataNascimento = dataNascimento;
+                Cadastro = cadastro;
+            }
+
+            public abstract void Cadastrar();
+        }
+
+        public class Cliente : Pessoa
+        {
+            public int Pontos { get; set; }
+
+            public Cliente(string nome, DateTime dataNascimento, int pontos, Cadastro cadastro)
+                : base(nome, dataNascimento, cadastro)
+            {
+                Pontos = pontos;
+            }
+
+            public override void Cadastrar()
+            {
+                Cadastro.AdicionarNaPlanilha("Clientes", new object[] { Nome, DataNascimento, Pontos, 0 });
+            }
+        }
+
+        public class Funcionario : Pessoa
+        {
+            public int Vendas { get; set; }
+            public string Estado { get; set; }
+
+            public Funcionario(string nome, DateTime dataNascimento, int vendas, string estado, Cadastro cadastro)
+                : base(nome, dataNascimento, cadastro)
+            {
+                Vendas = vendas;
+                Estado = estado;
+            }
+
+            public override void Cadastrar()
+            {
+                Cadastro.AdicionarNaPlanilha("Funcionarios", new object[] { Nome, DataNascimento, Vendas, Estado });
+            }
+        }
+
+
+        private void AdicionarNaPlanilha(string nomePlanilha, object[] dados)
+        {
+            string pastaAppData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string pastaMarket = Path.Combine(pastaAppData, "market");
+            string filePath = Path.Combine(pastaMarket, "dados.xlsx");
+
+            try
+            {
+                // Requisitar controle do semáforo
+                semaforo.WaitOne();
+
+                if (!File.Exists(filePath))
+                {
+                    throw new Exception("O arquivo dados.xlsx não foi encontrado.");
+                }
+
+                ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+
+                using (var package = new ExcelPackage(new FileInfo(filePath)))
+                {
+                    ExcelWorksheet planilha = package.Workbook.Worksheets[nomePlanilha];
+
+                    // Encontrar a próxima linha vazia
+                    int row = planilha.Dimension.End.Row + 1;
+
+                    // Definir o ID para a próxima linha
+                    planilha.Cells[row, 1].Value = row - 1;  // Supondo que a primeira linha seja para cabeçalho
+
+                    // Gravar os outros dados na planilha
+                    for (int i = 0; i < dados.Length; i++)
+                    {
+                        planilha.Cells[row, i + 2].Value = dados[i];
+                    }
+
+                    // Salvar as alterações
+                    package.Save();
+                }
+            }
+            finally
+            {
+                // Libera o semáforo
+                semaforo.Release();
+            }
         }
     }
 }
