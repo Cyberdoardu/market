a51088fdf276d947ff8b1fb2cb2bc9ef6eb710b9
davilson <daviventurini7@gmail.com>
2023-06-14 19:37:03 -0300

    aaaaa

--- market/Estoque.Designer.cs
+++ market/Estoque.Designer.cs
@@ -34,7 +34,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(367, 163);
+            this.label1.Location = new System.Drawing.Point(402, 76);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(57, 16);
             this.label1.TabIndex = 0;
